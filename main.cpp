922168dbec0b1540a2764a123eb7e8a0c1a6d91c
yahya-mohammed07 <goldroger300@gmail.com>
2020-09-30 17:27:55 +0300

    v_4

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <cstdint>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,7 +9,7 @@ auto longest (std::string dna, std::vector<std::string> str) -> void;
 
 auto main() -> int
 {
-    std::ifstream input ("sequences/5.txt");
+    std::ifstream input ("sequences/20.txt");
     std::string dna = "";
     if (input.is_open())
     {
@@ -18,7 +19,7 @@ auto main() -> int
         }
         input.close();
     }
-    else {std::cout << "error" << "\n";}
+    else {std::cout << "error" << "\n"; return 1;}
     //
     std::vector<std::string> strs {"AATG", "AGATC", "GAAA", "GATA", "TATC", "TCTAG", "TCTG", "TTTTTTCT"};
     longest (dna, strs);
@@ -26,15 +27,15 @@ auto main() -> int
 //
 auto longest (std::string dna, std::vector<std::string> str) -> void
 {
-    std::map<std::string, long> result;
-    long sizeDna = dna.size();
-    // FIXME: not works on small sequences
-    long temp = 0;
+    std::map<std::string, std::uint64_t> result;
+    std::uint64_t sizeDna = dna.size();
+    //
     for (const auto& s: str)
     {
-        long count = 0;
-        const long sizeStr = s.size();
-        long i = 0;
+        std::uint64_t temp = 0;
+        std::uint64_t count = 0;
+        const std::uint64_t sizeStr = s.size();
+        std::uint64_t i = 0;
         while (i < sizeDna)
         {
             std::string next = "";
@@ -48,14 +49,16 @@ auto longest (std::string dna, std::vector<std::string> str) -> void
                 i += sizeStr;
                 if (temp > count) {count = temp;}
             }
-            else {++i;temp = 0;}
+            else {++i; temp = 0;}
         }
         result.insert({s, count});
     }
     //
+    std::cout << '\n';
     for (auto const& i: result)
     {
-        std::cout << i.first << ": " << i.second << "\n";
+        std::cout <<" { "<< i.first << ": ";
+        std::cout << i.second << " }\n";
     }
     std::cout << '\n';
 }
